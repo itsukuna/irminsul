b00b2e3c0b03f934414bc514b87ad2b810d64202
Ayush Singh Arya <32599711+itsukuna@users.noreply.github.com>
2023-04-20 09:17:39 +0530

    optimized mobile viewing

--- components/navbar.js
+++ components/navbar.js
@@ -8,14 +8,21 @@ const Navbar = () => {
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex items-center justify-between h-16">
                     <div className="flex items-center">
-                        <div className="flex-shrink-0">
+                        <div className="flex-shrink-0 text-sm">
                             Irminsul
                         </div>
-                        <div className="hidden md:block">
-                            <div className="ml-10 flex items-baseline space-x-4">
-                                <Link href="/" className={`text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium ${router.pathname === '/' ? 'bg-gray-700 text-white' : ''}`}>Home</Link>
-                                <Link href="/artifacts" className={`text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium ${router.pathname === '/artifacts' ? 'bg-gray-700 text-white' : ''}`}>Artifacts</Link>
-                                <Link href="/weapons" className={`text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium ${router.pathname === '/weapons' ? 'bg-gray-700 text-white' : ''}`}>Weapons</Link>
+                        <div className="block sm:hidden ml-2">
+                            <button type="button" className="text-gray-300 hover:bg-gray-700 hover:text-white focus:outline-none focus:bg-gray-700 focus:text-white px-2 py-1 rounded-md">
+                                <svg className="h-6 w-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M4 6H20M4 12H20M4 18H20" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                                </svg>
+                            </button>
+                        </div>
+                        <div className="hidden sm:block">
+                            <div className="ml-4 flex items-baseline space-x-2">
+                                <Link href="/" className={`text-gray-300 hover:bg-gray-700 hover:text-white px-2 py-1 rounded-md text-xs font-medium ${router.pathname === '/' ? 'bg-gray-700 text-white' : ''}`}>Home</Link>
+                                <Link href="/artifacts" className={`text-gray-300 hover:bg-gray-700 hover:text-white px-2 py-1 rounded-md text-xs font-medium ${router.pathname === '/artifacts' ? 'bg-gray-700 text-white' : ''}`}>Artifacts</Link>
+                                <Link href="/weapons" className={`text-gray-300 hover:bg-gray-700 hover:text-white px-2 py-1 rounded-md text-xs font-medium ${router.pathname === '/weapons' ? 'bg-gray-700 text-white' : ''}`}>Weapons</Link>
                             </div>
                         </div>
                     </div>
--- components/weaponCard.js
+++ components/weaponCard.js
@@ -2,7 +2,7 @@ export default function WeaponCard({ name, weaponType, rarity, baseAttack, subSt
     const stars = "⭐️".repeat(rarity);
 
     return (
-        <div className="bg-gray-600 rounded-lg shadow-md hover:overflow-auto hover:transform hover:scale-105 transition duration-300 ease-in-out overflow-hidden" style={{ width: "300px", height: "400px", scrollbarWidth: "none" }}>
+        <div className="bg-gray-600 rounded-lg shadow-md hover:overflow-auto hover:transform hover:scale-105 transition duration-300 ease-in-out overflow-hidden" style={{ width: "100%", maxWidth: "300px", height: "400px", scrollbarWidth: "none" }}>
             <style>
                 {`::-webkit-scrollbar { display: none; }`}
             </style>
@@ -12,8 +12,8 @@ export default function WeaponCard({ name, weaponType, rarity, baseAttack, subSt
                 <div className="text-white mb-4"><span className="font-bold">{weaponType}</span> {stars}</div>
                 <div className="text-white mb-4"><span className="font-bold">Base Attack:</span> {baseAttack}</div>
                 <div className="text-white mb-4"><span className="font-bold">Sub Stat:</span> {subStat}</div>
-                <div className="text-white mb-4"><span className="font-bold">{passiveName}</span> </div>
-                <div className="text-white mb-4"><span className="font-bold"></span> {passiveDesc}</div>
+                <div className="text-white mb-4"><span className="font-bold">{passiveName}</span></div>
+                <div className="text-white mb-4">{passiveDesc}</div>
             </div>
         </div>
     );
